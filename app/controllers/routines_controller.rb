694dd9df34833606520a5bc2e45798660ccf41ce
Dusch4593 <brandondusch@gmail.com>
2020-05-29 11:23:29 -0400

    reconfigured app-route flow

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,4 +1,12 @@
 class RoutinesController < ApplicationController
   def new
   end
+
+  def index
+    if user_signed_in?
+      @routines = Routine.all
+    else
+      redirect_to welcome_page_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  # Top to Bottom --> Specific to Less Specific
+  get '/welcome', to: "welcome#home", as: "welcome_page"
   resources :routine_exercises
   resources :exercises
   resources :routines
-  root to: "welcome#home"
-  devise_for :users
-
   resources :routines do
     resources :exercises
   end
+
+  devise_for :users
+  root to: "routines#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
