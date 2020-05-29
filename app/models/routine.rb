484454a5ff93d6e9b3e60604e4dce0eac39ed883
Dusch4593 <brandondusch@gmail.com>
2020-05-29 11:35:12 -0400

    accepts nested attributes for child models

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,6 +1,7 @@
 class Routine < ApplicationRecord
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
+  accepts_nested_attributes_for :routine_exercises, :exercises
 
   validates :name, presence: true
 end
