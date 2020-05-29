0770db85530d5ce77f892a343b40de6dd0b64ade
Dusch4593 <brandondusch@gmail.com>
2020-05-29 08:22:27 -0400

    made migrations/ validations for user:name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :routines
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
