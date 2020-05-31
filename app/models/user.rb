82af500779cfad83861862e8db0ea17af4dfab3c
Dusch4593 <brandondusch@gmail.com>
2020-05-31 11:23:42 -0400

    added class-level method that extracts info from user auth via onmiauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  devise :omniauthable, omniauth_providers: %i[facebook, google, github]
+
   has_many :routines
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
