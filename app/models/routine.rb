966b18fa85c97e1e329a7b97771f1fa4cd305eca
Dusch4593 <brandondusch@gmail.com>
2020-05-30 22:29:20 -0400

    refactored how nested attributes are created

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,14 +2,14 @@ class Routine < ApplicationRecord
   belongs_to :users
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
-  accepts_nested_attributes_for :routine_exercises, :exercises
+  #accepts_nested_attributes_for :routine_exercises, :exercises
 
   validates :name, presence: true
 
-  # def exercises_attributes=(exercise_attributes)
-  # exercise_attributes.values.each do |exercise_attribute|
-  #   category = Exercise.find_or_create_by(exercise_attribute)
-  #   self.categories << category
-  # end
+  def exercises_attributes=(exercise_attributes)
+  exercise_attributes.values.each do |exercise_attribute|
+    category = Exercise.find_or_create_by(exercise_attribute)
+    self.exercises << exercise
+  end
 
 end
