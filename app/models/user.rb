6366d037b823e5a0e467ad2533717d2f8c985ea8
Dusch4593 <brandondusch@gmail.com>
2020-06-01 11:24:16 -0400

    DRYed out User.from_omniauth (provider and uid are already assigned)

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     #binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
