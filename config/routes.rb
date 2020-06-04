c3d553638aef89e84b183c8dfb74ff4e6c812ffb
Dusch4593 <brandondusch@gmail.com>
2020-06-04 19:57:54 -0400

    added nested routes for :routines and :exercises

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # Top to Bottom --> Specific to Less Specific
   get '/welcome', to: "welcome#home", as: "welcome_page"
 
-  resources :exercises
+
+  resources :routines, only: [:show] do
+    # nested resource for exercises
+    resources :exercises, only [:show, :index]
+  end
+
   resources :routines
-  resources :routine_exercises
+  resources :exercises
+
+
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root to: "welcome#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
