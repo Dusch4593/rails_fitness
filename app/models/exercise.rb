66aea51eae7de87ba7e17988be3a31296a5eec48
Dusch4593 <brandondusch@gmail.com>
2020-06-04 23:22:58 -0400

    exercise can now accepted nested attrs for routine exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,7 @@
 class Exercise < ApplicationRecord
   has_many :routine_exercises
   has_many :routines, through: :routine_exercises
+  accepts_nested_attributes_for :routine_exercises
 
   validates :name, presence: true
 end
