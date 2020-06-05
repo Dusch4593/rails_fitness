167d9155348c5671887329ceeb68606675a7e1e3
Dusch4593 <brandondusch@gmail.com>
2020-06-04 23:24:43 -0400

    updated #exercises_attributes

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,15 +1,20 @@
 class Routine < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
-  #accepts_nested_attributes_for :routine_exercises, :exercises
+  #accepts_nested_attributes_for :routine_exercises
 
   validates :name, presence: true
 
-  def exercises_attributes=(exercise_attributes)
-    exercise_attributes.values.each do |exercise_attribute|
-      exercise = Exercise.find_or_create_by(exercise_attribute)
-      self.exercises << exercise
-    end
+  def exercises_attributes=(exercise)
+    new_exercise = Exercise.find_or_create_by(name: exercise[:name])
+    self.exercises << new_exercise
   end
+
+  # def routine_exercises=(routine_exercise_attributes)
+  #   routine_exercise_attributes.values.each do |routine_exercise_attribute|
+  #     routine_exercise = RoutineExercise.find_or_create_by(routine_exercise_attribute)
+  #     self.routine_exercises << routine_exercise
+  #   end
+  # end
 end
