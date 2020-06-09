6d9ac3fb36d56b76244fc0fe23327a106233db85
Dusch4593 <brandondusch@gmail.com>
2020-06-09 13:29:15 -0400

    updated custom attributes setter for exercises_attributes

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,19 +2,13 @@ class Routine < ApplicationRecord
   belongs_to :user
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
-  #accepts_nested_attributes_for :routine_exercises
 
   validates :name, presence: true
 
   def exercises_attributes=(exercise)
-    new_exercise = Exercise.find_or_create_by(name: exercise[:name])
-    self.exercises << new_exercise
+    new_exercise = self.exercises.build
+    new_exercise.name = exercise[:name]
+    new_exercise.exercise_type = exercise[:exercise_type]
+    new_exercise.description = exercise[:description]
   end
-
-  # def routine_exercises=(routine_exercise_attributes)
-  #   routine_exercise_attributes.values.each do |routine_exercise_attribute|
-  #     routine_exercise = RoutineExercise.find_or_create_by(routine_exercise_attribute)
-  #     self.routine_exercises << routine_exercise
-  #   end
-  # end
 end
