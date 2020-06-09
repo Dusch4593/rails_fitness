1c751baa0ca7fc09632cf83f7888cfa2439da6b4
Dusch4593 <brandondusch@gmail.com>
2020-06-09 13:32:09 -0400

    added condition for rejecting empty nested attributes

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,5 @@
 class Exercise < ApplicationRecord
   has_many :routine_exercises
   has_many :routines, through: :routine_exercises
-  accepts_nested_attributes_for :routine_exercises
-
-  validates :name, presence: true
+  accepts_nested_attributes_for :routine_exercises, :reject_if => proc { |r_e| r_e[:sets].blank? }
 end
