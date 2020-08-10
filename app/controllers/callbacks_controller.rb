b9c401aea0d1ef9671cdac6289201526d653a2d6
Dusch4593 <brandondusch@gmail.com>
2020-08-10 10:54:30 -0400

    removed commented-out pry statements

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,11 +1,10 @@
 class CallbacksController < ApplicationController
+
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    #binding.pry
     if @user.persisted?
       sign_in_and_redirect @user
     else
-      # binding.pry
       flash[:error] = "There was a problem signing you in through Facebook. Please register or try signing in later."
       redirect_to new_user_registration_url
     end
